ad30757793ac2063d0b57c793119b1a2e1204d3c
Georgi Dinov <georgidinov1@abv.bg>
2020-07-26 13:07:19 +0300

    Adding Vets List ans updated controller . Closes #15.

--- pet-clinic-web/src/main/java/com/georgidinov/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/georgidinov/sfgpetclinic/controllers/VetController.java
@@ -1,14 +1,30 @@
 package com.georgidinov.sfgpetclinic.controllers;
 
+import com.georgidinov.sfgpetclinic.services.VetService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
-   @RequestMapping({"/vets","/vets/index", "/vets/index.html"})
-    public String vets(){
+    //== fields ==
+    private final VetService vetService;
+
+
+    //== constructors ==
+    @Autowired
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }//end of constructor
+
+
+    //== public methods ==
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
+    public String listVets(Model model) {
+        model.addAttribute("vets", this.vetService.findAll());
         return "vets/index";
-    }
+    }//end of method vets
 
 }//end of class VetController
