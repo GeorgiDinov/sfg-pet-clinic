c5790e24f7ddd4fca56768a33c539dcce3e5c957
Georgi Dinov <georgidinov1@abv.bg>
2020-07-28 14:53:00 +0300

    Added PetTypeService. Closes #29.

---
+++ pet-clinic-data/src/main/java/com/georgidinov/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package com.georgidinov.sfgpetclinic.services;
+
+import com.georgidinov.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long>{
+
+}//end of interface PetTypeService
