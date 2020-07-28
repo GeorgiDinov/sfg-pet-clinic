1e696761112c6d4fb735adadf3b37f85dab5028c
Georgi Dinov <georgidinov1@abv.bg>
2020-07-28 14:33:47 +0300

    Added Visit Entity updated Owner with Pet relationship. Closes #26.

--- pet-clinic-data/src/main/java/com/georgidinov/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/georgidinov/sfgpetclinic/model/Owner.java
@@ -1,5 +1,15 @@
 package com.georgidinov.sfgpetclinic.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+
 public class Owner extends Person {
+
+    @Getter
+    @Setter
+    private Set<Pet> pets;
 
 }//end of class Owner
