c62591c5b334372dbb164117d0167c6232b311d5
Georgi Dinov <georgidinov1@abv.bg>
2020-07-28 15:42:08 +0300

    Added Specialities Map Impl. Closes #31.

---
+++ pet-clinic-data/src/main/java/com/georgidinov/sfgpetclinic/services/SpecialitiesService.java
@@ -0,0 +1,7 @@
+package com.georgidinov.sfgpetclinic.services;
+
+import com.georgidinov.sfgpetclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+
+}//end of interface SpecialityService
