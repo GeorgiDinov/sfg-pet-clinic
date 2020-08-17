84de5c6e0e6518a2b55dda113bcdc169c0477ab3
Georgi Dinov <georgidinov1@abv.bg>
2020-08-17 15:24:53 +0300

    Convert Vet class with JPA entity mappings. Closes #37.

--- pet-clinic-data/src/main/java/com/georgidinov/sfgpetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/com/georgidinov/sfgpetclinic/model/Speciality.java
@@ -3,10 +3,17 @@ package com.georgidinov.sfgpetclinic.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specialities")
 public class Speciality extends BaseEntity {
 
     @Getter
     @Setter
+    @Column(name = "description")
     private String description;
 
 }//end of class Speciality
--- pet-clinic-data/src/main/java/com/georgidinov/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/georgidinov/sfgpetclinic/model/Vet.java
@@ -3,13 +3,24 @@ package com.georgidinov.sfgpetclinic.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
 
     @Getter
     @Setter
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specoalites", joinColumns = @JoinColumn(name = "vet_id"),
+    inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialities = new HashSet<>();
 
 }//end of class Vet
