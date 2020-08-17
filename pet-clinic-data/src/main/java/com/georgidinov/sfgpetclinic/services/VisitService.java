872cb56a5be05e58a8d4f5a304238abbd5233f41
Georgi Dinov <georgidinov1@abv.bg>
2020-08-17 23:20:03 +0300

    Implemented Map Impl for Visit Service. Closes #46.

---
+++ pet-clinic-data/src/main/java/com/georgidinov/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package com.georgidinov.sfgpetclinic.services;
+
+import com.georgidinov.sfgpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}//end of interface VisitService
