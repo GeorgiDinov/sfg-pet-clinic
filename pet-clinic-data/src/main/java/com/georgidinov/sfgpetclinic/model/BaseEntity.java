1f8a6affcee8d9702905f50b6c30152a059b5ab5
Georgi Dinov <georgidinov1@abv.bg>
2020-08-17 14:45:37 +0300

    Update BaseEntity class with JPA. Closes #35.

--- pet-clinic-data/src/main/java/com/georgidinov/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/georgidinov/sfgpetclinic/model/BaseEntity.java
@@ -3,8 +3,13 @@ package com.georgidinov.sfgpetclinic.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
     //== constants ==
@@ -13,6 +18,8 @@ public class BaseEntity implements Serializable {
     //== fields ==
     @Getter
     @Setter
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
 
