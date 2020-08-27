112a8a503c2500952a2412db565c645c42772ae3
Georgi Dinov <georgidinov1@abv.bg>
2020-08-27 13:27:32 +0300

    Added html files for owner. Closes #53.

--- pet-clinic-web/src/main/java/com/georgidinov/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/georgidinov/sfgpetclinic/controllers/OwnerController.java
@@ -4,7 +4,10 @@ import com.georgidinov.sfgpetclinic.services.OwnerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @RequestMapping("/owners")
 @Controller
@@ -29,8 +32,15 @@ public class OwnerController {
     }//end of method listOwners
 
     @RequestMapping({"/find"})
-    public String findOwners(){
+    public String findOwners() {
         return "notimplemented";
+    }
+
+    @GetMapping("/{ownerId}")
+    public ModelAndView showOwner(@PathVariable("ownerId") Long ownerId) {
+        ModelAndView mav = new ModelAndView("owners/ownerDetails");
+        mav.addObject(this.ownerService.findById(ownerId));
+        return mav;
     }
 
 
--- pet-clinic-web/src/test/java/com/georgidinov/sfgpetclinic/controllers/OwnerControllerTest.java
+++ pet-clinic-web/src/test/java/com/georgidinov/sfgpetclinic/controllers/OwnerControllerTest.java
@@ -15,7 +15,10 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import java.util.HashSet;
 import java.util.Set;
 
+import static org.hamcrest.Matchers.hasProperty;
 import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
@@ -72,5 +75,16 @@ class OwnerControllerTest {
                 .andExpect(view().name("notimplemented"));
         verifyNoInteractions(ownerService);
     }
+
+    @Test
+    void displayOwner() throws Exception {
+        when(ownerService.findById(anyLong())).thenReturn(Owner.builder().id(1L).build());
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/owners/123"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/ownerDetails"))
+                .andExpect(model().attribute("owner", hasProperty("id", is(1L))));
+    }
+
 
 }
